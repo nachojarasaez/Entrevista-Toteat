29051b42e591f5edbfd1c7e62ea25f80b724c3f4
nachojarasaez <injara@uc.cl>
2020-04-01 23:37:47 -0300

    feat: Initialize Project

--- src/routers/post/createWaiter.ts
+++ src/routers/post/createWaiter.ts
@@ -3,6 +3,7 @@ import { prisma } from '../../../prisma/generated/prisma-client';
 const createWaiter: any = async(req: any, res: any) => {
   const {waiter} = req.body;
   let newWaiter: any;
+  
   try {
     newWaiter = await prisma.createWaiter(waiter);
   } catch (e) {
